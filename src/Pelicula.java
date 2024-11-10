ea890feca778e2822f4c1982012b9b0686bda0e0
developerharo <developerharo@gmail.com>
2024-11-09 18:49:55 -0600

    Modificando los archivos de Clase Pelicula y Principal

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Pelicula {
+public class Pelicula {
+
+    String nombre;
+    int fechaDeLanzamiento;
+    int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+    double sumaDeLasEvaluaciones;
+    int totalDeLasEvaluaciones;
+
+    void muestraFichaTecnica(){
+        System.out.println("Nombre de la pelicula: " + nombre);
+        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Duración en minutos: " +duracionEnMinutos);
+    }
+
+    void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    double calculaMedia(){
+        return  sumaDeLasEvaluaciones/totalDeLasEvaluaciones;
+    }
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Principal {
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.nombre = "Encantada";
+        miPelicula.fechaDeLanzamiento = 2001;
+        miPelicula.duracionEnMinutos = 90;
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+
+        System.out.println(miPelicula.sumaDeLasEvaluaciones);
+        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        System.out.println(miPelicula.calculaMedia());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Piratas del Caribe";
+        otraPelicula.fechaDeLanzamiento = 2007;
+        otraPelicula.duracionEnMinutos = 110;
+
+        //otraPelicula.muestraFichaTecnica();
+
+    }
 }
